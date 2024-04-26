5ef6b939778ed7e5f83b68cdeae4b4e04e7e4770
fernando brito <130664535+britoGEMIDA@users.noreply.github.com>
2024-04-26 08:55:23 -0300

    comit

--- monckey-cassino/src/App.jsx
+++ monckey-cassino/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
+
+import React from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <header>
+        <h1>Monckey´s Luck</h1>
+      </header>
+      <main>
+        <section>
+          <h2>Fortune Tiger</h2>
+          {/* <ul>
+            <li>Jogo 1</li>
+            <li>Jogo 2</li>
+            <li>Jogo 3</li>
+          </ul> */}
+        </section>
+        <section>
+          <h2>Sobre Nós</h2>
+          <p>Descrição do cassino e seus serviços.</p>
+        </section>
+      </main>
+      <footer>
+        <p>&copy; Cassino Club</p>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
